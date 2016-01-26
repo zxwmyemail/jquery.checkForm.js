8bbb3d5dbfb1dc4f76b0f903158f2b04ce152cd5
iProg <1440552721@qq.com>
2016-01-26 14:59:45 +0800

    Update and rename jquery.iProgValidate.js to jquery.checkform.js

--- js/jquery.checkform.js
+++ js/jquery.checkform.js
@@ -13,13 +13,13 @@
  <span style="color:red" id="username-tip"></span>
 ******************************************************************************************************/
 ;(function($){
-    $.fn.formValidate = function (method) {
+    $.fn.checkform = function (method) {
         if (methods[method]) {
             return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
         } else if (typeof method === 'object' || !method) {
             return methods.init.apply(this, arguments);
         } else {
-            $.error('Method ' + method + ' does not exist on jQuery.formValidate');
+            $.error('Method ' + method + ' does not exist on jQuery.checkform');
         }
     };
 
@@ -370,7 +370,7 @@
     };
 
     // 默认参数
-    $.fn.formValidate.defaults = {
+    $.fn.checkform.defaults = {
         submitBtnId : false,
         onSubmitHandle : $.noop
     };
